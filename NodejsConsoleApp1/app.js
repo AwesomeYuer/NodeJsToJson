028678e3f43c554a92dcbbf266b459d34e3334f0
Microshaoft <microshaoft@msn.com>
2017-11-06 21:19:56 +0800

    .js

--- NodejsConsoleApp1/app.js
+++ NodejsConsoleApp1/app.js
@@ -1,11 +1,14 @@
 define = require('node-requirejs-define');
 'use strict';
 
-JsToJson('./options/option1');
-
 function JsToJson(source) {
-    var outputOptionFilePath = source.replace(".", "./output");
+    var outputOptionFilePath = source.replace("./", "./output/").replace(".js", ".json");
     var x = require(source);
+    var json = JSON.stringify(x, null, 4);
     var fs = require('fs');
-    fs.writeFile(outputOptionFilePath + '.json', JSON.stringify(x, null, 4));
+    fs.writeFile(outputOptionFilePath, json);
 }
+
+JsToJson('./options/option1.js');
+
+
