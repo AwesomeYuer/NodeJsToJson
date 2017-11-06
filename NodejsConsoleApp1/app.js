ce3c3b574f14f332dd27a5540ed02eb7a0ce87b7
Microshaoft <microshaoft@msn.com>
2017-11-06 20:35:33 +0800

    ok

--- NodejsConsoleApp1/app.js
+++ NodejsConsoleApp1/app.js
@@ -1,3 +1,11 @@
+define = require('node-requirejs-define');
 'use strict';
 
-console.log('Hello world');
+JsToJson('./options/option1');
+
+function JsToJson(source) {
+    var outputOptionFilePath = source.replace(".", "./output");
+    var x = require(source);
+    var fs = require('fs');
+    fs.writeFile(outputOptionFilePath + '.json', JSON.stringify(x, null, 4));
+}
